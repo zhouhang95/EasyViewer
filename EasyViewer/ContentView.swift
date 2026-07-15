a9d4d1c00748d979c9decadd43ecf5f15447217c
zhouhang95 <zhouhang95@foxmail.com>
2026-07-16 01:14:42 +0800

    Initial Commit

---
+++ EasyViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyViewer
+//
+//  Created by ZhouHang on 2026/7/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EasyViewer/EasyViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyViewerApp.swift
+//  EasyViewer
+//
+//  Created by ZhouHang on 2026/7/16.
+//
+
+import SwiftUI
+
+@main
+struct EasyViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
